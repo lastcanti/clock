80d4e84bed7451a719a6e2b4cc26ff292dd22441
Andy <lastcanti@gmail.com>
2018-01-27 10:23:37 -0500

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,85 @@
+hour = int(input("Enter an hour between 0-23____ "))
+mins = int(input("Enter a minute between 0-59____"))
+
+if hour == 0:
+    print ("It's twelve pm")
+elif hour == 1:
+	print ("It's one pm")
+elif hour == 2:
+	print ("It's two pm")
+elif hour == 3:
+        print ("It's three pm")
+elif hour == 4:
+	print ("It's four pm")
+elif hour == 5:
+	print ("It's five pm")
+elif hour == 6:
+	print ("It's six pm")
+elif hour == 7:
+	print ("It's seven pm")
+elif hour == 8:
+	print ("It's eight pm")
+elif hour == 9:
+	print ("It's nine pm")
+elif hour == 10:
+	print ("It's ten pm")
+elif hour == 11:
+	print ("It's eleven pm")
+elif hour == 12:
+	print ("It's twelve am")
+elif (hour - 12) == 0:
+    print ("It's twelve am")
+elif hour-12 == 1:
+	print ("It's one am")
+elif hour-12 == 2:
+	print ("It's two am")
+elif hour-12 == 3:
+        print ("It's three am")
+elif hour-12 == 4:
+	print ("It's four am")
+elif hour-12 == 5:
+	print ("It's five am")
+elif hour-12 == 6:
+	print ("It's six am")
+elif hour-12 == 7:
+	print ("It's seven am")
+elif hour-12 == 8:
+	print ("It's eight am")
+elif hour-12 == 9:
+	print ("It's nine am")
+elif hour-12 == 10:
+	print ("It's ten am")
+elif hour-12 == 11:
+	print ("It's eleven am")
+elif hour-12 == 12:
+	print ("It's twelve am")
+if mins == 1:
+    print ("oh one")
+elif mins == 2:
+    print("oh two")
+elif mins == 3:
+    print("oh three")
+elif mins == 4:
+    print("oh four")
+elif mins == 5:
+    print("oh five")
+elif mins == 6:
+    print("oh six")
+elif mins == 7:
+    print("oh seven")
+elif mins == 8:
+    print("oh eight")
+elif mins == 9:
+    print("oh nine")         
+if mins <= 10:
+    print("oh ten")
+elif mins <= 20:
+    print("oh twenty")
+elif mins <= 30:
+    print("oh thirty")
+elif mins <=40:
+    print("oh fourty")
+elif mins <= 50:
+    print("ohfifty")
+
+    
